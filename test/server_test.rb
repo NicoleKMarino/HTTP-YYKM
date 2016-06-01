911b167d1c25df62323deb6bc1ae5f580b4080ef
Chris Concannon <concach00@gmail.com>
2016-06-01 14:28:31 -0600

    first refactoring effort

---
+++ test/server_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "faraday"
+require "pry"
+
+class ServerTest < Minitest::Test
+  def test_response_body
+    response = Faraday.get('http://localhost:9292') 
+    assert response.body.include?("Hello, World!")
+  end
+
+end
